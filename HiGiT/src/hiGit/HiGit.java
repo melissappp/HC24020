bc60f7a48b0fd469d8f51e6527456f3ae812404b
melissappp <167501815+melissappp@users.noreply.github.com>
2024-04-25 21:22:50 -0600

    Update HiGit.java
    fix de mensaje

--- HiGiT/src/hiGit/HiGit.java
+++ HiGiT/src/hiGit/HiGit.java
@@ -3,7 +3,7 @@ package hiGit;
 public class HiGit {
 
        public static void master(string[] ares) {
-              system.out.print("Hellopeople");
+              //TODO auto-generate method stub
 
        }
 
